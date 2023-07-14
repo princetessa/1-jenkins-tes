b781b93e8ee830a518248d45de29791e093b8359
princetessa <128267325+princetessa@users.noreply.github.com>
2023-07-14 11:16:50 +0200

    Update Main.java
    ok

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("bungiorno mondo");
-        System.out.println("vladimir hhh");
+        System.out.println("vladimir hhhhdhbbd");
+        System.out.println("Hello world");
     }
 }
